cf883e3ce11fed638d332ea75eebd52f1ee8051e
morteza-jamali <mortezajamali4241@gmail.com>
2025-06-12 11:49:45 +0000

    feat: create sub_menu_1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.rtl.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './style.css';
+
+import $ from 'jquery';
+window.$ = $;
+
+$(function () {
+  $('.menu_btn').on('click', function () {
+    $(this).toggleClass('active');
+    $('.sub_menu_1').toggleClass('active');
+  });
+});
